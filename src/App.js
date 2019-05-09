2dbb40d70852318628c6cdb4c2b4a0eb6cd29a7b
erikyoung <erik.young@rmit.edu.vn>
2019-05-09 13:39:31 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Teachers from './components/Teachers'; 
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Howdy</h1>
+      <p>Test copy to see if font connects</p>
+      <button className="btn btn-primary">Test Button</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Teachers.js
@@ -0,0 +1,12 @@
+/* Core */
+import React, { Component } from 'react';
+
+
+
+export default class Teachers extends Component {
+  render() {
+    return (
+      <h1>This is the teachers page</h1>
+    );
+  }
+}
